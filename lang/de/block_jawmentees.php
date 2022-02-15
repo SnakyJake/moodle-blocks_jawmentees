f1c2672ed982962a5cd3b7c0e00d9a1a883b0e97
Jakob Heinemann <jakob@jakobheinemann.de>
2022-02-15 22:56:49 +0100

    Add files via upload

---
+++ lang/de/block_jawmentees.php
@@ -0,0 +1,32 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_jawmentees', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_jawmentees
+ * @copyright 2021 onwards Jakob Heinemann  {@link http://www.jakobheinemann.de}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configtitle'] = 'JAW Mentees Block Titel';
+$string['configtitleblankhides'] = 'JAW Mentees Block Titel (kein Titel wenn leer)';
+$string['mentees:addinstance'] = 'neuen JAW Mentees-Block hinzufügen';
+$string['mentees:myaddinstance'] = 'neuen JAW Mentees-Block zum Dashboard hinzufügen';
+$string['newmenteesblock'] = '(neuer JAW Mentees-Block)';
+$string['pluginname'] = 'JAW-Mentees';
+$string['privacy:metadata'] = 'Der JAW Mentees-Block zeigt nur an anderen Stellen gespeicherte Daten an.';
---
+++ lang/en/block_jawmentees.php
@@ -0,0 +1,32 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_mentees', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_jawmentees
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configtitle'] = 'JAW Mentees block title';
+$string['configtitleblankhides'] = 'JAW Mentees block title (no title if blank)';
+$string['mentees:addinstance'] = 'Add a new JAW mentees block';
+$string['mentees:myaddinstance'] = 'Add a new JAW mentees block to Dashboard';
+$string['newmenteesblock'] = '(new JAW Mentees block)';
+$string['pluginname'] = 'JAW Mentees';
+$string['privacy:metadata'] = 'The JAW Mentees block only shows data stored in other locations.';
